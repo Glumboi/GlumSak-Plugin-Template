ab17eeeb4dbb6cf066aefb5563804192964ea8f2
Glumboi <58010838+Glumboi@users.noreply.github.com>
2023-03-29 16:48:21 +0200

    Added UI Access to the Plugins

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using GlumSak_PluginTemplate.GlumSak;
 
 namespace GlumSak_PluginTemplate
 {
@@ -7,7 +10,7 @@ namespace GlumSak_PluginTemplate
     {
         public static int exitCode;
 
-        public static int PluginEntryPoint(object[] args) //--> Default Entrypoint of the Plugin
+        public static int PluginEntryPoint(IntPtr mainWindowhandle, object[] args) //--> Default Entrypoint of the Plugin
         {
             if (args.Length > 0 &&
                 args[0] is bool &&
@@ -16,8 +19,27 @@ namespace GlumSak_PluginTemplate
                 AppDomain.CurrentDomain.ProcessExit
                     += OverrrideExitEvent; //--> Use custom ExitEvent if first param is bool and true
             }
-
             //Your custom code here, you can also use WindowsForms with this (WPF might be possible too)
+
+            //UI access demo
+            MainWindowDummy mainWindowDummy = new MainWindowDummy(mainWindowhandle);
+
+            //Invoke to the UI Thread of GlumSak
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                //Show a notification in GlumSak
+                mainWindowDummy.Notification_SnackBar.Content = "This got changed from a plugin!";
+                mainWindowDummy.Notification_SnackBar.Show();
+
+                //Update the ProgressBar
+                mainWindowDummy.Progress_Border.Visibility = Visibility.Visible;
+                mainWindowDummy.Download_ProgressBar.Value = 50;
+                mainWindowDummy.DownloadProgress_TextBlock.Text = "Plugin Test";
+
+                //Example to get UIControls by their name, to know the names please reference GlumSaks source Files
+                var yuzuVerComboBox = mainWindowDummy.GetUiElement("YuzuVersions_DropDown") as ComboBox;
+                yuzuVerComboBox.SelectedIndex = 1;
+            }));
 
             exitCode = 0;
             return exitCode;
